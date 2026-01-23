e07b4ceeac84a1269504d7e97ea9fa803e10fcc6
Daniel Sandoval <gil_orrori_94@hotmail.com>
2026-01-22 19:51:14 -0600

    Se agrega singleton ya que se llama en diferntes sitios esta clase con la DI y no funciona como se espera

--- app/src/main/java/com/gilorroristore/quotesmvvm/data/network/provider/QuoteProvider.kt
+++ app/src/main/java/com/gilorroristore/quotesmvvm/data/network/provider/QuoteProvider.kt
@@ -2,9 +2,9 @@ package com.gilorroristore.quotesmvvm.data.network.provider
 
 import com.gilorroristore.quotesmvvm.data.network.model.QuoteModel
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class QuoteProvider @Inject constructor() {
-        companion object {
-        var quotes: List<QuoteModel> = emptyList()
-    }
+    var quotes: List<QuoteModel> = emptyList()
 }
